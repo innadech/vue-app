1438e9300dfc33546ab776171b9bacadaab7efe5
yourock1988 <113207809+yourock1988@users.noreply.github.com>
2025-06-28 21:51:40 +0300

    Refs

--- src/components/FruitSubmitter.vue
+++ src/components/FruitSubmitter.vue
@@ -10,8 +10,11 @@ export default {
 
   methods: {
     handleClickButton() {
-      this.$emit('fruit-submitted', this.fruit)
-      this.fruit = ''
+      if (this.fruit !== '') {
+        this.$emit('fruit-submitted', this.fruit)
+        this.fruit = ''
+      }
+      this.$refs.elInput.focus()
     },
   },
 }
@@ -20,6 +23,7 @@ export default {
 <template>
   <div>
     <input
+      ref="elInput"
       type="text"
       v-bind:value="fruit"
       v-on:input="fruit = $event.target.value"
